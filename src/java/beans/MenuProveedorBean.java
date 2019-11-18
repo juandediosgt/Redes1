6420865d961836fa836248d94f0bdf12cfe6db95
codejuandedios <codejuandedios@gmail.com>
2019-11-18 14:01:36 -0600

    Interfaz Proveedores

---
+++ src/java/beans/MenuProveedorBean.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package beans;
+
+/**
+ *
+ * @author Juan de Dios
+ */
+public class MenuProveedorBean {
+
+    /**
+     * Creates a new instance of MenuProveedorBean
+     */
+    public MenuProveedorBean() {
+    }
+    public String irNuevoProveedor(){
+       String resultado="nuevo";
+               return resultado;
+   }
+    public String irConsultarProveedor(){
+       String resultado="consulta";
+               return resultado;
+   }
+    public String irActualizarProveedor(){
+       String resultado="actualizar";
+               return resultado;
+   }
+    public String irmenuPrincipal(){
+       String resultado="regresar";
+               return resultado;
+   }
+    
+}
---
+++ src/java/pojos/Proveedor.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pojos;
+
+/**
+ *
+ * @author Juan de Dios
+ */
+public class Proveedor {
+
+    /**
+     * Creates a new instance of Proveedor
+     */
+    public Proveedor() {
+    }
+    
+}
